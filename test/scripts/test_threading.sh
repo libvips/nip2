9914244e99651aaa54ff133a586a1309e816c646
John Cupitt <jcupitt@gmail.com>
2011-08-03 10:14:45 +0100

    better threading test
    use im_benchmarkn as a threading test, uses more operations ... also
    test a greater range of tile sizes and cpu numbers

--- test/scripts/test_threading.sh
+++ test/scripts/test_threading.sh
@@ -3,41 +3,39 @@
 top_srcdir=$1
 tmp=$top_srcdir/test/tmp
 image=$top_srcdir/share/nip2/data/examples/businesscard/slanted_oval_vase2.jpg
+chain=1
 
-# write with a certain tilesize and number of threads, test avg 
-write_test() {
-	image=$1
-	threads=$2
-	tilew=$3
-	tileh=$4
-	avg=$5
+# im_benchmark needs a labq
+vips im_sRGB2XYZ $image $tmp/t1.v
+vips im_XYZ2Lab $tmp/t1.v $tmp/t2.v
+vips im_Lab2LabQ $tmp/t2.v $tmp/t3.v
 
-	args="--vips-concurrency=$threads \
-		--vips-tile-width=$tilew \
-		--vips-tile-height=$tileh"
+for tile in 10 64 128 512; do
+	# benchmark includes a dither which will vary with tile size
+	vips --vips-concurrency=1 \
+		--vips-tile-width=$tile --vips-tile-height=$tile \
+		im_benchmarkn $tmp/t3.v $tmp/t4.v $chain
 
-	echo -n "testing threads=$threads, tilew=$tilew, tileh=$tileh ... "
-
-	if ! vips $args im_rot90 $image $tmp/t1.v ; then
-		echo "write failed"
-		exit 1
+	for cpus in 2 3 4 5 6 7 8 99 1024; do
+		echo trying cpus = $cpus, tile = $tile ..
+		vips --vips-concurrency=$cpus \
+			--vips-tile-width=$tile --vips-tile-height=$tile \
+			im_benchmarkn $tmp/t3.v $tmp/t5.v $chain
+		vips im_subtract $tmp/t4.v $tmp/t5.v $tmp/t6.v
+		vips im_abs $tmp/t6.v $tmp/t7.v
+		max=`vips im_max $tmp/t7.v`
+		if [[ $max > 0 ]]; then
+			break
+		fi
+	done
+	if [[ $max > 0 ]]; then
+		break
 	fi
+done
 
-	result=`vips $args im_avg $tmp/t1.v`
-
-	if [ $result != $avg ] ; then
-		echo "result should be $avg, was $result"
-		exit 1
-	fi
-
-	echo ok
-}
-
-avg=`vips im_avg $image`
-
-write_test $image 1 64 64 $avg
-write_test $image 1 256 256 $avg
-write_test $image 10 256 256 $avg
-write_test $image 1024 256 256 $avg
-write_test $image 1024 16 16 $avg
+if [[ $max > 0 ]]; then
+	echo error, max == $max
+else
+	echo all threading tests passed
+fi
 

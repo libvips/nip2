0202e1ae5765a00abe907ef48f73b2a08f4514ca
John Cupitt <jcupitt@gmail.com>
2012-01-12 14:00:40 +0000

    more format test improvements

--- test/scripts/test_formats.sh
+++ test/scripts/test_formats.sh
@@ -8,8 +8,8 @@ test_images=$top_srcdir/test/images
 image=$test_images/slanted_oval_vase2.jpg
 
 # the matlab image and reference image
-matlab=$test_images/test/images/lena.mat
-matlab_ref=$test_images/test/images/lena.tif
+matlab=$test_images/lena.mat
+matlab_ref=$test_images/lena.tif
 
 # make a mono image
 vips im_extract_band $image $tmp/mono.v 1
@@ -41,6 +41,16 @@ save_load() {
 	fi
 }
 
+# is a difference beyond a threshold? return 0 (meaning all ok) or 1 (meaning
+# error, or outside threshold)
+# 
+# use bc since bash does not support fp math
+break_threshold() {
+	diff=$1
+	threshold=$2
+	return $(echo "$diff <= $threshold" | bc -l)
+}
+
 # subtract, look for max difference less than a threshold
 test_difference() {
 	before=$1
@@ -51,12 +61,7 @@ test_difference() {
 	vips im_abs $tmp/difference.v $tmp/abs.v 
 	dif=`vips im_max $tmp/abs.v`
 
-	# vips8 max will display "0.000" not zero, which will confuse the
-	# int-only bash
-	#
-	# make something with bc to do this comparison instead?
-
-	if (( $dif > $threshold )) ; then
+	if break_threshold $dif $threshold; then
 		echo "save / load difference is $dif"
 		exit 1
 	fi
@@ -70,7 +75,7 @@ test_format() {
 	threshold=$3
 	mode=$4
 
-	echo -n "testing $in $format$mode ... "
+	echo -n "testing $(basename $in) $format$mode ... "
 
 	save_load $in $format $mode
 	test_difference $in $tmp/back.v $threshold
@@ -84,7 +89,7 @@ test_format() {
 test_rad() {
 	in=$1
 
-	echo -n "testing $in hdr ... "
+	echo -n "testing $(basename $in) hdr ... "
 
 	save_load $in hdr
 
@@ -101,7 +106,7 @@ test_rad() {
 test_raw() {
 	in=$1
 
-	echo -n "testing $in raw ... "
+	echo -n "testing $(basename $in) raw ... "
 
 	vips copy $in $tmp/before.v
 	width=`vips im_header_int Xsize $tmp/before.v`
@@ -121,8 +126,9 @@ test_raw() {
 test_loader() {
 	ref=$1
 	in=$2
+	format=$3
 
-	echo -n "testing $in loader ... "
+	echo -n "testing $(basename $in) $format ... "
 
 	vips copy $ref $tmp/before.v
 	vips copy $in $tmp/after.v
@@ -134,14 +140,14 @@ test_loader() {
 
 test_format $image v 0
 test_format $image tif 0
-test_format $image tif 10 :jpeg
+test_format $image tif 15 :jpeg
 test_format $image tif 0 :deflate
 test_format $image tif 0 :packbits
-test_format $image tif 10 :jpeg,tile
-test_format $image tif 10 :jpeg,tile,pyramid
+test_format $image tif 15 :jpeg,tile
+test_format $image tif 15 :jpeg,tile,pyramid
 test_format $image png 0
 test_format $image png 0 :9,1
-test_format $image jpg 10
+test_format $image jpg 15
 test_format $image ppm 0
 test_format $image pfm 0
 test_format $image fits 0
@@ -150,18 +156,18 @@ test_format $image fits 0
 test_format $mono csv 0
 
 # cmyk jpg is a special path
-test_format $cmyk jpg 10
+test_format $cmyk jpg 15
 test_format $cmyk tif 0
-test_format $cmyk tif 10 :jpeg
-test_format $cmyk tif 10 :jpeg,tile
-test_format $cmyk tif 10 :jpeg,tile,pyramid
+test_format $cmyk tif 15 :jpeg
+test_format $cmyk tif 15 :jpeg,tile
+test_format $cmyk tif 15 :jpeg,tile,pyramid
 
 test_rad $rad 
 
 test_raw $mono 
 test_raw $image 
 
-test_loader $matlab_ref $matlab
+test_loader $matlab_ref $matlab matlab
 
 # we have loaders but not savers for other formats, add tests here
 

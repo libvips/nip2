af0017264ff0cca0a2082926805ec269dff4b920
John Cupitt <jcupitt@gmail.com>
2013-10-18 09:32:49 +0100

    fix bootstrap warnings
    for ubuntu 13.10

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,25 @@ rm -rf autom4te.cache
 rm -f install-sh intltool-* ltmain.sh missing mkinstalldirs
 rm -f src/*.o src/nip2 src/Makefile src/Makefile.in 
 
-# exit
+# glib-gettextize asks us to copy these files to m4 if they aren't there
+# I don't have $ACDIR/isc-posix.m4, how mysterious
+ACDIR=`aclocal --print-ac-dir`
+
+# OS X with brew sets ACDIR to
+# /usr/local/Cellar/automake/1.13.1/share/aclocal, the staging area, which is
+# totally wrong argh
+if [ ! -d $ACDIR ]; then
+	ACDIR=/usr/local/share/aclocal
+fi
+
+mkdir -p m4
+cp $ACDIR/codeset.m4 m4
+cp $ACDIR/gettext.m4 m4
+cp $ACDIR/glibc21.m4 m4
+cp $ACDIR/iconv.m4 m4
+cp $ACDIR/isc-posix.m4 m4
+cp $ACDIR/lcmessage.m4 m4
+cp $ACDIR/progtest.m4 m4
 
 # some systems need libtoolize, some glibtoolize ... how annoying
 echo testing for glibtoolize ...

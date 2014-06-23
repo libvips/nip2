da16765af85d2426e16fca6a57de9004cda9278f
John Cupitt <jcupitt@gmail.com>
2014-06-23 20:22:28 +0100

    bump version to 7.40

--- test/scripts/test_seq.sh
+++ test/scripts/test_seq.sh
@@ -17,7 +17,7 @@ huge=$tmp/huge.png
 echo -n "testing vipsthumbnail ... "
 rm -f $tmp/x.png
 vipsthumbnail $huge -o $tmp/x.png
-if ! header $tmp/x.png &> /dev/null ; then
+if ! vipsheader $tmp/x.png &> /dev/null ; then
 	echo "vipsthumbnail failed in basic mode"
 	exit 1
 fi
@@ -32,7 +32,7 @@ export TMPDIR=$tmp/readonly
 echo -n "testing vipsthumbnail does not make temps ... "
 rm -f $tmp/x.png
 vipsthumbnail $huge -o $tmp/x.png
-if ! header $tmp/x.png &> /dev/null ; then
+if ! vipsheader $tmp/x.png &> /dev/null ; then
 	echo "vipsthumbnail made a temp"
 	exit 1
 fi
@@ -41,7 +41,7 @@ echo "ok"
 echo -n "testing shrink does not make temps ... "
 rm -f $tmp/x.png
 vips shrink $huge $tmp/x.png 230 230 
-if ! header $tmp/x.png &> /dev/null; then
+if ! vipsheader $tmp/x.png &> /dev/null; then
 	echo "shrink made a temp"
 	exit 1
 fi
